ad127f163c2a01716479be8f06066c97299bcebe
Victor Kachin <victor.kachin@gmail.com>
2022-09-08 15:55:43 +0300

    Create and Style the End Page

--- game.js
+++ game.js
@@ -1,6 +1,8 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -43,7 +45,6 @@ startGame = () => {
 	questionCounter = 0;
 	score = 0;
 	availableQuesions = [...questions];
-	// console.log(availableQuesions);
 	getNewQuestion();
 };
 
@@ -53,6 +54,10 @@ getNewQuestion = () => {
 		return window.location.assign('/end.html');
 	}
 	questionCounter++;
+	progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+	//Update the progress bar
+	progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
 	const questionIndex = Math.floor(Math.random() * availableQuesions.length);
 	currentQuestion = availableQuesions[questionIndex];
 	question.innerText = currentQuestion.question;
@@ -68,32 +73,31 @@ getNewQuestion = () => {
 
 choices.forEach(choice => {
 	choice.addEventListener('click', e => {
-		// console.log(e.target);
-
 		if (!acceptingAnswers) return;
 
 		acceptingAnswers = false;
 		const selectedChoice = e.target;
 		const selectedAnswer = selectedChoice.dataset['number'];
 
-		// const classToApply = 'incorrect'
-		// if (selectedAnswer == currentQuestion.answer) {
-		//     classToApply = 'correct'
-		// }
-
 		const classToApply =
 			selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
-		// console.log(classToApply)
+		if (classToApply === 'correct') {
+			incrementScore(CORRECT_BONUS);
+		}
 
 		selectedChoice.parentElement.classList.add(classToApply);
 
 		setTimeout(() => {
 			selectedChoice.parentElement.classList.remove(classToApply);
-
 			getNewQuestion();
 		}, 1000);
 	});
 });
+
+incrementScore = num => {
+	score += num;
+	scoreText.innerText = score;
+};
 
 startGame();
